b5c76171b11c39f330235f60d324e98c719a54b3
amaasik <75305698+amaasik@users.noreply.github.com>
2023-01-26 14:07:01 +0200

    RealEstateInterface

--- TARge21Shop/TARge21Shop.ApplicationServices/Services/RealEstatesServices.cs
+++ TARge21Shop/TARge21Shop.ApplicationServices/Services/RealEstatesServices.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TARge21Shop.Core.Domain;
+using TARge21Shop.Core.Dto;
+using TARge21Shop.Core.ServiceInterface;
 using TARge21Shop.Data;
 
 namespace TARge21Shop.ApplicationServices.Services
 {
    
-    public class RealEstatesServices
+    public class RealEstatesServices : IRealEstates
     {
         private readonly TARge21ShopContext _context;
 
@@ -27,6 +29,29 @@ namespace TARge21Shop.ApplicationServices.Services
                 .FirstOrDefaultAsync(x => x.Id == id);
 
             return result;
+        }
+        public async Task<RealEstate>Create(RealEstateDto dto)
+        {
+            RealEstate realEstate = new();
+
+            realEstate.Id = Guid.NewGuid();
+            realEstate.Address = dto.Address;
+            realEstate.City = dto.City;
+            realEstate.Region= dto.Region;
+            realEstate.PostalCode= dto.PostalCode;
+            realEstate.Country= dto.Country;
+            realEstate.Phone = dto.Phone;
+            realEstate.Fax = dto.Fax;
+            realEstate.Size= dto.Size;
+            realEstate.Floor= dto.Floor;
+            realEstate.Price= dto.Price;
+            realEstate.RoomCount= dto.RoomCount;
+            realEstate.CreatedAt= DateTime.Now;
+            realEstate.ModifiedAt= DateTime.Now;
+
+            await _context.RealEstates.AddAsync(realEstate);
+            await _context.SaveChangesAsync();
+            return realEstate;
         }
     }
 }
--- TARge21Shop/TARge21Shop.Core/ServiceInterface/IRealEstates.cs
+++ TARge21Shop/TARge21Shop.Core/ServiceInterface/IRealEstates.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TARge21Shop.Core.Domain;
 
 namespace TARge21Shop.Core.ServiceInterface
 {
     public interface IRealEstates
     {
+        Task<RealEstate> GetAsync(Guid id);
     }
 }
